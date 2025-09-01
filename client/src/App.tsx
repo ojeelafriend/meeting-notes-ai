e5ab1bf6ae587fc869f2ec32bd8e9702b9d476c0
odev <ojeelafriend@gmail.com>
2025-09-01 16:48:19 -0400

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-gray-900">
+        <h1 className="text-4xl font-bold text-white">Hello Tailwind</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
