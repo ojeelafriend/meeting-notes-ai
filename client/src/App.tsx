4ddbbd646a1bf4146eac79156fc8f28d7c5fdfcb
odev <ojeelafriend@gmail.com>
2025-09-01 18:51:22 -0400

    [modify] diseño base del mvp

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,39 @@
-import "./App.css";
+import { BiCloudUpload } from "react-icons/bi";
+import folderIcon from "./assets/folder.png";
 
 function App() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen bg-gray-900">
-        <h1 className="text-4xl font-bold text-white">Hello Tailwind</h1>
+      {/* Header */}
+      <header className="header">
+        <div className="header-content">
+          <h2 className="header-title">Neural Tools</h2>
+          <div className="header-buttons">
+            <button className="btn-primary">Summarize</button>
+            <button className="btn-secondary">List summaries</button>
+          </div>
+        </div>
+      </header>
+
+      <div className="page">
+        <h1 className="text-white">Meeting Notes AI</h1>
+        <div className="glass">
+          <img src={folderIcon} alt="folder" height={100} width={100} />
+          <h1>Upload files </h1>
+          <p>Select and upload the files of your choice</p>
+          <div className="inner-glass">
+            <input type="file" multiple hidden />
+
+            <BiCloudUpload size={30} color="gray" />
+            <p className="text-explained">
+              Choose a file or drag and drop it here
+            </p>
+            <p className="text-format-supported">
+              MP4, MP3 formats supported, up to 100MB
+            </p>
+            <button className="browse-file">Browse File</button>
+          </div>
+        </div>
       </div>
     </>
   );
