828b0676a3cbe28289090ab6be6928f15d68805f
odev <ojeelafriend@gmail.com>
2025-09-02 16:32:11 -0400

    implemento react router y templeate hader fixed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,35 @@
-import { BiCloudUpload } from "react-icons/bi";
-import folderIcon from "./assets/folder.png";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-function App() {
+import Home from "./components/Home";
+import GridNotes from "./components/GridNotes";
+import "./index.css";
+
+export default function App() {
+  const navigate = useNavigate();
   return (
     <>
-      {/* Header */}
       <header className="header">
         <div className="header-content">
-          <h2 className="header-title">Neural Tools</h2>
+          <h2 className="header-title">Neural Tools AI</h2>
           <div className="header-buttons">
-            <button className="btn-primary">Summarize</button>
-            <button className="btn-secondary">List summaries</button>
+            <button className="btn-primary" onClick={() => navigate("/")}>
+              Summarize
+            </button>
+            <button
+              className="btn-secondary"
+              onClick={() => navigate("/notes")}
+            >
+              List summaries
+            </button>
           </div>
         </div>
       </header>
-
-      <div className="page">
-        <h1 className="text-white">Meeting Notes AI</h1>
-        <div className="glass">
-          <img src={folderIcon} alt="folder" height={100} width={100} />
-          <h1>Upload files </h1>
-          <p>Select and upload the files of your choice</p>
-          <div className="inner-glass">
-            <input type="file" multiple hidden />
-
-            <BiCloudUpload size={30} color="gray" />
-            <p className="text-explained">
-              Choose a file or drag and drop it here
-            </p>
-            <p className="text-format-supported">
-              MP4, MP3 formats supported, up to 100MB
-            </p>
-            <button className="browse-file">Browse File</button>
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/notes" element={<GridNotes />} />
+      </Routes>
     </>
   );
 }
-
-export default App;
